91b4373bfa4b0f42f0d9502390258402a019f838
Mikael Svensson <mikael.svensson@klarna.com>
2017-07-29 18:47:15 +0200

    Fix incorrect URL in test

--- server-application/src/test/java/se/devscout/achievements/server/OrganizationsAcceptanceTest.java
+++ server-application/src/test/java/se/devscout/achievements/server/OrganizationsAcceptanceTest.java
@@ -3,7 +3,6 @@ package se.devscout.achievements.server;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.eclipse.jetty.http.HttpStatus;
-import org.glassfish.jersey.logging.LoggingFeature;
 import org.junit.ClassRule;
 import org.junit.Test;
 import se.devscout.achievements.server.api.OrganizationDTO;
@@ -12,28 +11,32 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
 import java.net.URI;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class OrganizationsAcceptanceTest {
     @ClassRule
     public static final DropwizardAppRule<AchievementsApplicationConfiguration> RULE =
-            new DropwizardAppRule<>(AchievementsApplication.class, ResourceHelpers.resourceFilePath("server-test-configuration.yaml"));
+            new DropwizardAppRule<>(
+                    AchievementsApplication.class,
+                    ResourceHelpers.resourceFilePath("server-test-configuration.yaml"));
 
     @Test
     public void create_happyPath() {
         Client client = RULE.client();
 
         Response response = client
-                .target(String.format("http://localhost:%d/organizations", RULE.getLocalPort()))
+                .target(String.format("http://localhost:%d/api/organizations", RULE.getLocalPort()))
                 .request()
                 .post(Entity.json(new OrganizationDTO(null, "Name")));
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED_201);
+
         final OrganizationDTO responseDto = response.readEntity(OrganizationDTO.class);
-        assertThat(response.getLocation()).isEqualTo(URI.create(String.format("http://localhost:%d/organizations/%s", RULE.getLocalPort(), responseDto.id)));
+
+        final URI expectedLocation = URI.create(String.format("http://localhost:%d/api/organizations/%s", RULE.getLocalPort(), responseDto.id));
+        final URI actualLocation = response.getLocation();
+        assertThat(actualLocation).isEqualTo(expectedLocation);
     }
 
 }
