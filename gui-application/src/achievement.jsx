3a5bb6ec92091d590bcd312b076b280480da32d2
Mikael Svensson <mikael.svensson@klarna.com>
2017-10-08 21:17:06 +0200

    Add GUI support for setting progress

--- gui-application/src/achievement.jsx
+++ gui-application/src/achievement.jsx
@@ -7,39 +7,57 @@ const templateLoading = require("./loading.handlebars");
 
 export function renderAchievement(appPathParams) {
     updateView(templateLoading());
-    get('//localhost:8080/api/achievements/' + appPathParams[0].key, function (responseData, responseStatus, jqXHR) {
-        responseData.breadcrumbs = [
+    get('//localhost:8080/api/achievements/' + appPathParams[0].key, function (achievementData, responseStatus, jqXHR) {
+        let templateData = achievementData;
+        templateData.breadcrumbs = [
             {label: "Hem", url: '#/'},
             {label: "Märken och bedrifter", url: '#achievements/'},
-            {label: responseData.name}
+            {label: achievementData.name}
         ];
 
-        updateView(templateAchievement(responseData));
+        get('//localhost:8080/api/my/people/', function (peopleData, responseStatus, jqXHR) {
 
-        $('#app').find('.create-step-button').click(function (e) {
-            const button = $(this);
-            const form = button.addClass('is-loading').closest('form');
-            post('//localhost:8080/api/achievements/' + appPathParams[0].key + '/steps', getFormData(form), function (responseData, responseStatus, jqXHR) {
-                button.removeClass('is-loading');
-                get('//localhost:8080/api/achievements/' + appPathParams[0].key + "/steps", function (responseData, responseStatus, jqXHR) {
-                    updateView(templateAchievementStepsList({
-                        steps: responseData,
-                        achievementId: appPathParams[0].key
-                    }), $('#achievement-steps-list'));
+            updateView(templateAchievement(templateData));
+
+            $('#app').find('.create-step-button').click(function (e) {
+                const button = $(this);
+                const form = button.addClass('is-loading').closest('form');
+                post('//localhost:8080/api/achievements/' + appPathParams[0].key + '/steps', getFormData(form), function (responseData, responseStatus, jqXHR) {
+                    button.removeClass('is-loading');
+                    get('//localhost:8080/api/achievements/' + appPathParams[0].key + "/steps", function (responseData, responseStatus, jqXHR) {
+                        updateView(templateAchievementStepsList({
+                            steps: responseData,
+                            achievementId: appPathParams[0].key
+                        }), $('#achievement-steps-list'));
+                    });
                 });
             });
-        });
 
-        // TODO: Perhaps populate form using any of the solutions on https://stackoverflow.com/questions/9807426/use-jquery-to-re-populate-form-with-json-data or https://stackoverflow.com/questions/7298364/using-jquery-and-json-to-populate-forms instead?
-        $.each(responseData, function (key, value) {
-            $('#' + key).val(value);
-        });
 
-        get('//localhost:8080/api/achievements/' + appPathParams[0].key + "/steps", function (responseData, responseStatus, jqXHR) {
-            updateView(templateAchievementStepsList({
-                steps: responseData,
-                achievementId: appPathParams[0].key
-            }), $('#achievement-steps-list'));
+            // TODO: Perhaps populate form using any of the solutions on https://stackoverflow.com/questions/9807426/use-jquery-to-re-populate-form-with-json-data or https://stackoverflow.com/questions/7298364/using-jquery-and-json-to-populate-forms instead?
+            $.each(achievementData, function (key, value) {
+                $('#' + key).val(value);
+            });
+
+            get('//localhost:8080/api/achievements/' + appPathParams[0].key + "/steps", function (responseData, responseStatus, jqXHR) {
+                updateView(templateAchievementStepsList({
+                    people: peopleData,
+                    steps: responseData,
+                    achievementId: appPathParams[0].key
+                }), $('#achievement-steps-list'));
+
+                $(".progress-switch").click(function () {
+                    const toggleButton = $(this);
+                    toggleButton.addClass('is-loading');
+                    const toggleCompletedUrl = '//localhost:8080/api/achievements/' + appPathParams[0].key + '/steps/' + this.dataset.stepId + '/person/' + this.dataset.personId;
+                    const completed = (!toggleButton.hasClass('is-danger') && !toggleButton.hasClass('is-success')) || toggleButton.hasClass('is-danger');
+                    post(toggleCompletedUrl, {"completed": completed}, function (responseData, responseStatus, jqXHR) {
+                        toggleButton.removeClass('is-loading');
+                        toggleButton.removeClass(responseData.completed ? 'is-danger' : 'is-success');
+                        toggleButton.addClass(responseData.completed ? 'is-success' : 'is-danger');
+                    });
+                });
+            });
         });
     });
 }
