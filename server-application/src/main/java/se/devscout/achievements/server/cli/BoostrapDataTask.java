e914698c69ead6673b26224432ea63f3ba41a474
Mikael Svensson <mikael.svensson@klarna.com>
2017-10-08 21:17:15 +0200

    Add bootstrap data for a couple of achievements

--- server-application/src/main/java/se/devscout/achievements/server/cli/BoostrapDataTask.java
+++ server-application/src/main/java/se/devscout/achievements/server/cli/BoostrapDataTask.java
@@ -3,23 +3,24 @@ package se.devscout.achievements.server.cli;
 import com.google.common.collect.ImmutableMultimap;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import se.devscout.achievements.server.data.dao.OrganizationsDao;
-import se.devscout.achievements.server.data.dao.PeopleDao;
-import se.devscout.achievements.server.data.model.Organization;
-import se.devscout.achievements.server.data.model.OrganizationProperties;
-import se.devscout.achievements.server.data.model.Person;
-import se.devscout.achievements.server.data.model.PersonProperties;
+import se.devscout.achievements.server.data.dao.*;
+import se.devscout.achievements.server.data.model.*;
 
 import java.io.PrintWriter;
+import java.util.Collections;
 
 public class BoostrapDataTask extends DatabaseTask {
     private final OrganizationsDao organizationsDao;
     private final PeopleDao peopleDao;
+    private final AchievementsDao achievementsDao;
+    private final AchievementStepsDao achievementStepsDao;
 
-    public BoostrapDataTask(SessionFactory sessionFactory, OrganizationsDao organizationsDao, PeopleDao peopleDao) {
+    public BoostrapDataTask(SessionFactory sessionFactory, OrganizationsDao organizationsDao, PeopleDao peopleDao, AchievementsDao achievementsDao, AchievementStepsDao achievementStepsDao) {
         super("bootstrap-data", sessionFactory);
         this.organizationsDao = organizationsDao;
         this.peopleDao = peopleDao;
+        this.achievementsDao = achievementsDao;
+        this.achievementStepsDao = achievementStepsDao;
     }
 
     @Override
@@ -30,9 +31,18 @@ public class BoostrapDataTask extends DatabaseTask {
         createPerson(output, organization, "Randall Boggs");
         createPerson(output, organization, "Celia Mae");
         createPerson(output, organization, "Roz");
+
+        final Achievement achievementBike = createAchievement(output, "Ride bike", "easy");
+        createAchievementStep(output, achievementBike, "Get the bike");
+        createAchievementStep(output, achievementBike, "Learn to ride it");
+        final Achievement achievementMotorcycle = createAchievement(output, "Ride motorcycle", "easy");
+        createAchievementStep(output, achievementMotorcycle, achievementBike);
+        createAchievementStep(output, achievementMotorcycle, "Study the traffic laws");
+        createAchievementStep(output, achievementMotorcycle, "Pass driving exam");
+        createAchievementStep(output, achievementMotorcycle, "Pass written exam");
     }
 
-    private Organization createOrganization(PrintWriter output, String name) throws se.devscout.achievements.server.data.dao.DaoException {
+    private Organization createOrganization(PrintWriter output, String name) throws DaoException {
         final Organization organization = organizationsDao.create(new OrganizationProperties(name));
         output.format("Created organization %s (id %s)%n", organization.getName(), organization.getId());
         return organization;
@@ -41,5 +51,23 @@ public class BoostrapDataTask extends DatabaseTask {
     private void createPerson(PrintWriter output, Organization organization, String name) {
         final Person person = peopleDao.create(organization, new PersonProperties(name));
         output.format("Created person %s (id %s)%n", person.getName(), person.getId());
+    }
+
+    private AchievementStep createAchievementStep(PrintWriter output, Achievement achievement, String description) {
+        final AchievementStep step = achievementStepsDao.create(achievement, new AchievementStepProperties(description));
+        output.format("Created regular achievement step (id %s)%n", step.getId());
+        return step;
+    }
+
+    private AchievementStep createAchievementStep(PrintWriter output, Achievement achievement, Achievement prerequisiteAchievement) {
+        final AchievementStep step = achievementStepsDao.create(achievement, new AchievementStepProperties(prerequisiteAchievement));
+        output.format("Created prerequisite achievement step (id %s)%n", step.getId());
+        return step;
+    }
+
+    private Achievement createAchievement(PrintWriter output, String name, String tag) throws DaoException {
+        final Achievement achievement = achievementsDao.create(new AchievementProperties(name, Collections.singleton(tag)));
+        output.format("Created achievement %s (id %s)%n", achievement.getName(), achievement.getId());
+        return achievement;
     }
 }
