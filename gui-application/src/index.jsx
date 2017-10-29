5cf33489c9db000cede2012f41f12e8013f7f05b
Mikael Svensson <mikael.svensson@klarna.com>
2017-10-29 21:03:00 +0100

    Rotate table headings 45 degrees

--- gui-application/src/index.jsx
+++ gui-application/src/index.jsx
@@ -1,11 +1,13 @@
 import $ from "jquery";
 import styles from "bulma/css/bulma.css";
 import {hashChangeHandler} from "./util/routing.jsx";
+import tableStyles from "./table.css";
 
 $(function () {
     $(window).on('hashchange', hashChangeHandler);
 
     const s = styles;
+    const ts = tableStyles;
 
     $(window).trigger('hashchange');
 });
