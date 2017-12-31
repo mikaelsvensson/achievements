e2ed49b168f09c40ace35674496d9db4d24e1b57
Mikael Svensson <mikael.svensson@klarna.com>
2017-12-31 08:32:11 +0100

    Unify design of the three sign-in pages

--- gui-application/src/auth/auth.redirect.jsx
+++ gui-application/src/auth/auth.redirect.jsx
@@ -2,9 +2,6 @@ import {setToken} from "../util/api.jsx";
 import {navigateTo} from "../util/routing.jsx";
 
 export function renderRedirect(appPathParams) {
-
-    console.log("Got parameters:", appPathParams)
-
     setToken(appPathParams[0].key);
 
     navigateTo('minprofil');
