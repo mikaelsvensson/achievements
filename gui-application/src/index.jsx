d9db7af7d3e792bc2384e385c721c5ce7733487f
Mikael Svensson <mikael.svensson@klarna.com>
2018-01-04 23:03:10 +0100

    Use custom font Scouterna

--- gui-application/src/index.jsx
+++ gui-application/src/index.jsx
@@ -2,12 +2,14 @@ import $ from "jquery";
 import {hashChangeHandler} from "./util/routing.jsx";
 import tableStyles from "./table.css";
 import mdiStyles from "mdi/css/materialdesignicons.css";
+import themeFonts from "./assets/font/stylesheet.css";
 import themeStyles from "./scout-theme.scss";
 
 $(function () {
     $(window).on('hashchange', hashChangeHandler);
 
     const ms = themeStyles;
+    const tf = themeFonts;
     const ts = tableStyles;
     const mdis = mdiStyles;
 
